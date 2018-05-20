c2504e68d5c8f8eb6a97f77beaaf634c7e436aa5
yogurt_lei <yogurt_lei@foxmail.com>
2018-05-20 16:26:56 +0800

    components

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,41 @@
+<!--图标组件 使用方法 <svg-icon icon-class="password"></svg-icon> -->
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+<script>
+  export default {
+    name: 'svg-icon',
+    props: {
+      iconClass: {
+        type: String,
+        required: true
+      },
+      className: {
+        type: String
+      }
+    },
+    computed: {
+      iconName() {
+        return `#icon-${this.iconClass}`
+      },
+      svgClass() {
+        if (this.className) {
+          return 'svg-icon ' + this.className
+        } else {
+          return 'svg-icon'
+        }
+      }
+    },
+  }
+</script>
+<style scoped>
+  .svg-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
